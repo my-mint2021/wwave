03cda4adf7053d41e447be5d349673768e9c67cd
my-mint2021 <81299128+my-mint2021@users.noreply.github.com>
2021-05-06 00:22:24 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,9 +8,19 @@ $(function () {
     // autoplaySpeed:4000,
   });
 
-$('.menu__burger').on('click', function(){
-$('.menu').toggleClass('menu--active');
+// $('.menu__burger').on('click', function(){
+// $('.menu').toggleClass('menu--active');
+// $('body').toggleClass('lock');
+// });
+
+$(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger').toggleClass('header__burger--active');
+		$('.menu').toggleClass('menu--active');
+		$('body').toggleClass('body--lock');
+	});
 });
+
 
   var mixer = mixitup('.gallery__content');
 });
