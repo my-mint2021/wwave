90b211f39abf82a814785926f04fb753bc4c792a
my-mint2021 <81299128+my-mint2021@users.noreply.github.com>
2021-05-08 14:09:13 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () {
  
-  $('.blog__slider').slick({
+  $('.blog-slider').slick({
     dots: true,
     arrows: false,
   });
@@ -18,6 +18,36 @@ $( '.menu' ).on("click", function(){
   $('.menu').removeClass('menu--active');
   $('body').removeClass('body--lock');
 });
+
+$(".menu a, footer a").on("click", function (e) {
+  e.preventDefault();
+  var id  = $(this).attr('href'),
+      top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 1500);
+});
+  
+// $(window).scroll(function(){
+//   if ($(this).scrollTop() > 500) {
+//       $('.header__top').addClass('header__top--fixed');
+//   } else {
+//       $('.header__top').removeClass('header__top--fixed');
+//   }
+// });
+
+var header = $('.header__top'),
+		scrollPrev = 0;
+
+$(window).scroll(function() {
+	var scrolled = $(window).scrollTop();
+ 
+	if ( scrolled > 600 && scrolled > scrollPrev ) {
+		header.addClass('header__top--fixed');
+	} else {
+		header.removeClass('header__top--fixed');
+	}
+	scrollPrev = scrolled;
+});
+
 
   var mixer = mixitup('.gallery__content');
 });
